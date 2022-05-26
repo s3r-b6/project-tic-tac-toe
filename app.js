cd82358380d98fcadcbda921721c5a56cfa14814
s3r-b6 <103552324+s3r-b6@users.noreply.github.com>
2022-05-26 17:43:34 +0200

    lint

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
+/* jshint esversion: 6 */
 window.onload = () => {
   //modules
-  let turn = 0
+  let turn = 0;
   //el tablero es un array de 3 arrays (rows)
   let gameBoard = [
     [' ', ' ', ' '],
     [' ', ' ', ' '],
-    [' ', ' ', ' ']
-  ]
+    [' ', ' ', ' '],
+  ];
   let clearBoard = [
     [' ', ' ', ' '],
     [' ', ' ', ' '],
-    [' ', ' ', ' ']
-  ]
+    [' ', ' ', ' '],
+  ];
 
   function drawBoard() {
-    document.querySelector("#display").innerHTML = ''
-    document.querySelector("#display").innerHTML += `
+    document.querySelector('#display').innerHTML = '';
+    document.querySelector('#display').innerHTML += `
     <div id="screen"> texto de prueba </div>
     <div id="boardContainer">
       <div class='row r0'>
@@ -34,43 +35,48 @@ window.onload = () => {
         <p class="gridContent c2">${gameBoard[2][2]}</p>
       </div>
     </div>
-      `
-    game()
+      `;
+    game();
   }
 
   function game() {
     //descubre qué casilla se ha clickado y devuelve la posición
-    document.querySelectorAll(".gridContent").forEach((el) => {
-      el.addEventListener("click", (e) => {
+    document.querySelectorAll('.gridContent').forEach((el) => {
+      el.addEventListener('click', (e) => {
         console.log(e.target);
 
-        let row = e.target.parentNode.classList[1][1]
-        let column = e.target.classList[1][1]
+        let row = e.target.parentNode.classList[1][1];
+        let column = e.target.classList[1][1];
         console.log(row, column);
 
         //descubre si la casilla está vacía o no
-        let currValue = gameBoard[row][column]
+        let currValue = gameBoard[row][column];
         if (currValue == ' ') {
           if (turn % 2 == 0) {
-            gameBoard[row][column] = '⭕'
-            turn++
+            gameBoard[row][column] = '⭕';
+            turn++;
           } else if (!turn % 2 == 0) {
-            gameBoard[row][column] = '❌'
-            turn++
+            gameBoard[row][column] = '❌';
+            turn++;
           }
-          checkWinner()
-          drawBoard()
+
+          checkWinner();
+          drawBoard();
         }
         //si la casilla está tomada, añade la clase taken a la casilla y luego de 650 ms la retira
-        else if (!currValue == 'X' || 'O') {
-          document.querySelector(`.r${row} > .c${column}`).classList.add('taken')
+        else if (currValue !== 'X' || currValue !== 'O') {
+          document
+            .querySelector(`.r${row} > .c${column}`)
+            .classList.add('taken');
           setTimeout(() => {
-            document.querySelector(`.r${row} > .c${column}`).classList.remove('taken')
-          }, 650)
-          console.log('casilla ocupada')
+            document
+              .querySelector(`.r${row} > .c${column}`)
+              .classList.remove('taken');
+          }, 650);
+          console.log('casilla ocupada');
         }
-      })
-    })
+      });
+    });
   }
 
   function checkWinner() {
@@ -86,8 +92,9 @@ window.onload = () => {
       gameBoard[0][2] == '⭕' && gameBoard[1][1] == '⭕' && gameBoard[0][2] == '⭕' ||
       gameBoard[0][0] == '⭕' && gameBoard[1][1] == '⭕' && gameBoard[2][2] == '⭕'
     ) {
-      let winner = 'winner: ⭕'
-      console.log(winner)
+      let winner = 'winner: ⭕';
+      turn = 0;
+      console.log(winner);
 
     } else if ( //rows
       gameBoard[0][0] == '❌' && gameBoard[0][1] == '❌' && gameBoard[0][2] == '❌' ||
@@ -101,16 +108,17 @@ window.onload = () => {
       gameBoard[0][2] == '❌' && gameBoard[1][1] == '❌' && gameBoard[0][2] == '❌' ||
       gameBoard[0][0] == '❌' && gameBoard[1][1] == '❌' && gameBoard[2][2] == '❌'
     ) {
-      let winner = 'winner: ❌'
-      console.log(winner)
+      let winner = 'winner: ❌';
+      turn = 0;
+      console.log(winner);
 
     } else {
       return;
     }
-    //pop del ganador
-    console.log('test test test')
 
+    //pop del ganador
+    console.log('test test test');
   }
 
-  drawBoard()
-}
+  drawBoard();
+};
