f923005600105850a46c5a6856b8ec792887a1bc
s3r-b6 <103552324+s3r-b6@users.noreply.github.com>
2022-05-31 10:24:09 +0200

    code cleanup & style fix

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 window.onload = () => {
-  //modules
-  let turn = 0;
-
   //el tablero es un array de 3 arrays (rows)
+  let turn = 0;
   let gameBoard = [
     [' ', ' ', ' '],
     [' ', ' ', ' '],
@@ -16,6 +14,7 @@ window.onload = () => {
       document.querySelector('#display').innerHTML += `
         <div class='row r${r}'></div> `;
       document.querySelector("#display > div.row.r0")
+
       for (c in gameBoard[r]) {
         //console.log('col:' + c)
         document.querySelector(`.r${r}`).innerHTML += `
@@ -23,15 +22,14 @@ window.onload = () => {
       `
       }
     };
-    game();
+    startGame();
   }
 
-  function game() {
+  function startGame() {
     //descubre qué casilla se ha clickado y devuelve la posición
     document.querySelectorAll('.gridContent').forEach((el) => {
       el.addEventListener('click', (e) => {
         // console.log(e.target);
-
         let row = e.target.parentNode.classList[1][1];
         let column = e.target.classList[1][1];
         // console.log(row, column);
@@ -42,65 +40,69 @@ window.onload = () => {
           if (turn % 2 == 0) {
             gameBoard[row][column] = '⭕';
             turn++;
+            drawBoard()
+            checkWinner()
           } else if (!turn % 2 == 0) {
             gameBoard[row][column] = '❌';
             turn++;
+            drawBoard();
+            checkWinner();
           }
-          checkWinner();
-          drawBoard();
-        }
-
-        //si la casilla está tomada, añade la clase taken a la casilla y luego de 650 ms la retira
-        else if (currValue == '❌' || currValue == '⭕') {
+        } else if (currValue == '❌' || currValue == '⭕') {
           document.querySelector(`.r${row} > .c${column}`).classList.add('taken');
           setTimeout(() => {
             document.querySelector(`.r${row} > .c${column}`).classList.remove('taken');
           }, 650);
-          // console.log('casilla ocupada');
         }
       });
     });
   }
 
   function checkWinner() {
-    if ( //rows
-      gameBoard[0][0] == '⭕' && gameBoard[0][1] == '⭕' && gameBoard[0][2] == '⭕' ||
-      gameBoard[1][0] == '⭕' && gameBoard[1][1] == '⭕' && gameBoard[1][2] == '⭕' ||
-      gameBoard[2][0] == '⭕' && gameBoard[2][1] == '⭕' && gameBoard[2][2] == '⭕' ||
-      //columns
-      gameBoard[0][0] == '⭕' && gameBoard[1][0] == '⭕' && gameBoard[2][0] == '⭕' ||
-      gameBoard[0][1] == '⭕' && gameBoard[1][1] == '⭕' && gameBoard[2][1] == '⭕' ||
-      gameBoard[0][2] == '⭕' && gameBoard[1][2] == '⭕' && gameBoard[2][2] == '⭕' ||
-      //diagonal
-      gameBoard[0][2] == '⭕' && gameBoard[1][1] == '⭕' && gameBoard[2][0] == '⭕' ||
-      gameBoard[0][0] == '⭕' && gameBoard[1][1] == '⭕' && gameBoard[2][2] == '⭕'
-    ) {
-      let winner = 'winner: ⭕';
-      console.log(winner);
-
-    } else if ( //rows
-      gameBoard[0][0] == '❌' && gameBoard[0][1] == '❌' && gameBoard[0][2] == '❌' ||
-      gameBoard[1][0] == '❌' && gameBoard[1][1] == '❌' && gameBoard[1][2] == '❌' ||
-      gameBoard[2][0] == '❌' && gameBoard[2][1] == '❌' && gameBoard[2][2] == '❌' ||
-      //columns
-      gameBoard[0][0] == '❌' && gameBoard[1][0] == '❌' && gameBoard[2][0] == '❌' ||
-      gameBoard[0][1] == '❌' && gameBoard[1][1] == '❌' && gameBoard[2][1] == '❌' ||
-      gameBoard[0][2] == '❌' && gameBoard[1][2] == '❌' && gameBoard[2][2] == '❌' ||
-      //diagonal
-      gameBoard[0][2] == '❌' && gameBoard[1][1] == '❌' && gameBoard[2][0] == '❌' ||
-      gameBoard[0][0] == '❌' && gameBoard[1][1] == '❌' && gameBoard[2][2] == '❌'
-    ) {
-      let winner = 'winner: ❌';
-      console.log(winner);
-
-    } else {
-      return;
+    function checkRow(row) {
+      let rowValues = gameBoard[row];
+      let rowWinner = rowValues.join('');
+      if (rowWinner == '⭕⭕⭕' || rowWinner == '❌❌❌') {
+        console.log('rowWinner: ' + rowWinner);
+      }
     }
-    //pop del ganador
-    console.log('test test test');
-    document.querySelector('#restartButton')
 
+    function checkColumn(column) {
+      let columnValues = [];
+      for (let i = 0; i < gameBoard.length; i++) {
+        columnValues.push(gameBoard[i][column]);
+      }
+      let columnWinner = columnValues.join('');
+      if (columnWinner == '⭕⭕⭕' || columnWinner == '❌❌❌') {
+        console.log('columnWinner: ' + columnWinner);
+      }
+    }
 
+    function checkDiagonal(diagonal) {
+      let diagonalValues = [];
+      if (diagonal == 'topLeftToBottomRight') {
+        for (let i = 0; i < gameBoard.length; i++) {
+          diagonalValues.push(gameBoard[i][i]);
+        }
+      } else if (diagonal == 'topRightToBottomLeft') {
+        for (let i = 0; i < gameBoard.length; i++) {
+          diagonalValues.push(gameBoard[i][gameBoard.length - 1 - i]);
+        }
+      }
+      let diagonalWinner = diagonalValues.join('');
+      if (diagonalWinner == '⭕⭕⭕' || diagonalWinner == '❌❌❌') {
+        console.log('diagonalWinner: ' + diagonalWinner);
+      }
+    }
+    for (let i = 0; i < gameBoard.length; i++) {
+      checkRow(i);
+      checkColumn(i);
+      checkDiagonal('topLeftToBottomRight');
+      checkDiagonal('topRightToBottomLeft');
+    }
+    if (turn == 9) {
+      console.log('Draw!');
+    }
   }
 
   document.querySelector('#restartButton').addEventListener('click', () => {
