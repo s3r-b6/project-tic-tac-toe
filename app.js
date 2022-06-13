083af3729ccbf54a8c5542ba352843a6b82eb65c
s3r-b6 <103552324+s3r-b6@users.noreply.github.com>
2022-06-13 08:00:51 +0200

    several fixes && winner screen
    small refactor + winner screen; changed spanish comments to english;
    project is finished for now (maybe future revisit but pretty much done; there are many things that could have been done better but overall pretty useful project to learn)

--- app.js
+++ app.js
@@ -1,120 +1,155 @@
 window.onload = () => {
-  //el tablero es un array de 3 arrays (rows)
-  let turn = 0;
-  let gameBoard = [
-    [' ', ' ', ' '],
-    [' ', ' ', ' '],
-    [' ', ' ', ' '],
-  ];
+  drawBoard();
+  restartHandler();
+};
 
-  function drawBoard() {
-    document.querySelector('#display').innerHTML = '';
-    for (r in gameBoard) {
-      //console.log('row:' + r)
-      document.querySelector('#display').innerHTML += `
+//the board is a 2d array, winner is a flag to check if the game has ended, turn is a counter, used to check whose turn it is and if there has been a tie
+let turn = 0;
+let winner = false;
+let gameBoard = [
+  [" ", " ", " "],
+  [" ", " ", " "],
+  [" ", " ", " "],
+];
+
+function drawBoard() {
+  document.querySelector("#display").innerHTML = "";
+  for (r in gameBoard) {
+    document.querySelector("#display").innerHTML += `
         <div class='row r${r}'></div> `;
-      document.querySelector("#display > div.row.r0")
+    document.querySelector("#display > div.row.r0");
 
-      for (c in gameBoard[r]) {
-        //console.log('col:' + c)
-        document.querySelector(`.r${r}`).innerHTML += `
+    for (c in gameBoard[r]) {
+      document.querySelector(`.r${r}`).innerHTML += `
             <p class="gridContent c${c}">${gameBoard[r][c]}</p>
-      `
-      }
-    };
-    startGame();
+      `;
+    }
   }
+  drawPick();
 
-  function startGame() {
-    //descubre qué casilla se ha clickado y devuelve la posición
-    document.querySelectorAll('.gridContent').forEach((el) => {
-      el.addEventListener('click', (e) => {
-        // console.log(e.target);
+  function drawPick() {
+    //find out what box has been clicked and return that
+    document.querySelectorAll(".gridContent").forEach((el) => {
+      el.addEventListener("click", handleBoxClick());
+    });
+
+    function handleBoxClick() {
+      return (e) => {
         let row = e.target.parentNode.classList[1][1];
         let column = e.target.classList[1][1];
-        // console.log(row, column);
-
-        //descubre si la casilla está vacía o no
+        //check if the box is empty or not
         let currValue = gameBoard[row][column];
-        if (currValue == ' ') {
+        if (currValue == " " && winner == false) {
           if (turn % 2 == 0) {
-            gameBoard[row][column] = '⭕';
+            gameBoard[row][column] = "⭕";
             turn++;
-            drawBoard()
-            checkWinner()
+            drawBoard();
+            checkWinner();
           } else if (!turn % 2 == 0) {
-            gameBoard[row][column] = '❌';
+            gameBoard[row][column] = "❌";
             turn++;
             drawBoard();
             checkWinner();
           }
-        } else if (currValue == '❌' || currValue == '⭕') {
-          document.querySelector(`.r${row} > .c${column}`).classList.add('taken');
+        } else if (currValue == "❌" || currValue == "⭕") {
+          document
+            .querySelector(`.r${row} > .c${column}`)
+            .classList.add("taken");
           setTimeout(() => {
-            document.querySelector(`.r${row} > .c${column}`).classList.remove('taken');
+            document
+              .querySelector(`.r${row} > .c${column}`)
+              .classList.remove("taken");
           }, 650);
         }
-      });
-    });
-  }
 
-  function checkWinner() {
-    function checkRow(row) {
-      let rowValues = gameBoard[row];
-      let rowWinner = rowValues.join('');
-      if (rowWinner == '⭕⭕⭕' || rowWinner == '❌❌❌') {
-        console.log('rowWinner: ' + rowWinner);
-      }
-    }
+        //check for every win-case
+        function checkWinner() {
+          for (let i = 0; i < gameBoard.length; i++) {
+            checkRow(i);
+            checkColumn(i);
+            checkDiagonal("topLeftToBottomRight");
+            checkDiagonal("topRightToBottomLeft");
 
-    function checkColumn(column) {
-      let columnValues = [];
-      for (let i = 0; i < gameBoard.length; i++) {
-        columnValues.push(gameBoard[i][column]);
-      }
-      let columnWinner = columnValues.join('');
-      if (columnWinner == '⭕⭕⭕' || columnWinner == '❌❌❌') {
-        console.log('columnWinner: ' + columnWinner);
-      }
-    }
+            function checkRow(row) {
+              let rowValues = gameBoard[row];
+              let rowWinner = rowValues.join("");
+              if (rowWinner == "⭕⭕⭕" || rowWinner == "❌❌❌") {
+                stopGame(rowWinner);
+              }
+            }
 
-    function checkDiagonal(diagonal) {
-      let diagonalValues = [];
-      if (diagonal == 'topLeftToBottomRight') {
-        for (let i = 0; i < gameBoard.length; i++) {
-          diagonalValues.push(gameBoard[i][i]);
+            function checkColumn(column) {
+              let columnValues = [];
+              for (let i = 0; i < gameBoard.length; i++) {
+                columnValues.push(gameBoard[i][column]);
+              }
+              let columnWinner = columnValues.join("");
+              if (columnWinner == "⭕⭕⭕" || columnWinner == "❌❌❌") {
+                stopGame(columnWinner);
+              }
+            }
+
+            function checkDiagonal(diagonal) {
+              let diagonalValues = [];
+              if (diagonal == "topLeftToBottomRight") {
+                for (let i = 0; i < gameBoard.length; i++) {
+                  diagonalValues.push(gameBoard[i][i]);
+                }
+              } else if (diagonal == "topRightToBottomLeft") {
+                for (let i = 0; i < gameBoard.length; i++) {
+                  diagonalValues.push(gameBoard[i][gameBoard.length - 1 - i]);
+                }
+              }
+              let diagonalWinner = diagonalValues.join("");
+              if (diagonalWinner == "⭕⭕⭕" || diagonalWinner == "❌❌❌") {
+                stopGame(diagonalWinner);
+              }
+            }
+            //if it is turn 9 and there is no winner it means there is a tie
+            if (turn == 9 && winner == false) {
+              stopGame(false);
+            }
+          }
+
+          function stopGame(who) {
+            let winnerScreen = document.createElement("div");
+            winnerScreen.id = "winnerScreen";
+            winnerScreen.classList.add("hidden");
+            if (who) {
+              winnerScreen.innerHTML = `
+                <h2> ${who} wins the game! </h2>
+                `;
+            } else {
+              winnerScreen.innerHTML = `
+                <h2> It is a draw! </h2>
+                `;
+            }
+            document.querySelector("main").append(winnerScreen);
+            winner = true;
+            setTimeout(() => {
+              winnerScreen.classList.remove("hidden");
+            }, 100);
+          }
         }
-      } else if (diagonal == 'topRightToBottomLeft') {
-        for (let i = 0; i < gameBoard.length; i++) {
-          diagonalValues.push(gameBoard[i][gameBoard.length - 1 - i]);
-        }
-      }
-      let diagonalWinner = diagonalValues.join('');
-      if (diagonalWinner == '⭕⭕⭕' || diagonalWinner == '❌❌❌') {
-        console.log('diagonalWinner: ' + diagonalWinner);
-      }
-    }
-    for (let i = 0; i < gameBoard.length; i++) {
-      checkRow(i);
-      checkColumn(i);
-      checkDiagonal('topLeftToBottomRight');
-      checkDiagonal('topRightToBottomLeft');
-    }
-    if (turn == 9) {
-      console.log('Draw!');
+      };
     }
   }
-
-  document.querySelector('#restartButton').addEventListener('click', () => {
-    document.querySelector('#display').innerHTML = '';
+}
+function restartHandler() {
+  document.querySelector("#restartButton").addEventListener("click", () => {
+    if (document.querySelector("#winnerScreen")) {
+      document.querySelector("#winnerScreen").classList.add("hidden");
+      setTimeout(() => {
+        document.querySelectorAll("#winnerScreen").forEach((el) => el.remove());
+      }, 300);
+    }
     gameBoard = [
-      [' ', ' ', ' '],
-      [' ', ' ', ' '],
-      [' ', ' ', ' ']
+      [" ", " ", " "],
+      [" ", " ", " "],
+      [" ", " ", " "],
     ];
     turn = 0;
+    winner = false;
     drawBoard();
   });
-
-  drawBoard();
-};
+}
